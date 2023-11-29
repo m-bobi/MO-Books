5a928e0e6dd8392a1ea759eff1c450874625e976
m-bobi <melosbobi11@gmail.com>
2023-11-29 14:04:24 +0100

    + Changed book Covers, added some shadows

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-// -----------------------     CAROUSEL (slider)
-
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) { slideIndex = 1 }
-    // x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-
-
 // -----------------------    NAVBAR RESPONSIVENESS
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -45,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // ---------------------- USER VALIDATION
 
 document.addEventListener('DOMContentLoaded', function () {
+    \
     const accountCreated = localStorage.getItem('accountCreated');
 
     if (accountCreated === 'true') {
