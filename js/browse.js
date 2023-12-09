2edc57e6ea0f92e5148721a8698d5f1d2676ac61
OltiBuleshka <oltibuleshka24@gmail.com>
2023-12-09 23:11:46 +0100

    new patch 2

--- js/browse.js
+++ js/browse.js
@@ -43,28 +43,27 @@ function carousel() {
 // ---------------------- ADD TO CART FUNCTIONALITY
 
 function addToCart(productId, productName, productPrice, productImage) {
-    // Get the cart data from localStorage
+
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-    // Check if the product is already in the cart
     const existingProduct = cart.find(item => item.id === productId);
 
     if (existingProduct) {
-        // If the product is already in the cart, update the quantity
+
         existingProduct.quantity += 1;
     } else {
-        // If the product is not in the cart, add it
+
         const newProduct = {
             id: productId,
             name: productName,
             price: productPrice,
             quantity: 1,
-            image: productImage, // Add the image property
+            image: productImage,
         };
         cart.push(newProduct);
     }
 
-    // Save the updated cart data to localStorage
+
     localStorage.setItem('cart', JSON.stringify(cart));
     alert('Product added to cart!');
 }
@@ -75,14 +74,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const accountCreated = localStorage.getItem('accountCreated');
 
     if (accountCreated === 'true') {
-        // User has created an account, replace the user image
-        // Assuming 'userImage' is the ID of the image element
         const userImage = document.getElementById('userImage');
-
-        // Update the image source
         userImage.src = '/resources/logos/panda.png';
-
-        // Apply styles to the userImage
         userImage.style.backgroundColor = '#C05A34';
         userImage.style.borderRadius = '60px';
         userImage.style.height = '60px';
@@ -90,3 +83,4 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+```
